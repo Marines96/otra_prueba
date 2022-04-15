46f99bca7b4cad815afb4f391acd86370caaf93c
Marines96 <103769658+Marines96@users.noreply.github.com>
2022-04-15 18:15:45 -0400

    Update main.py

--- main.py
+++ main.py
@@ -238,7 +238,7 @@ def onmessage(update,bot:ObigramClient):
         tl_admin_user = os.environ.get('tl_admin_user')
 
         #set in debug
-        tl_admin_user = 'FriendXfriendss'
+        tl_admin_user = 'Marines123'
 
         jdb = JsonDatabase('database')
         jdb.check_create()
